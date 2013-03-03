92d8efd60c4b48e1c92a7c806524799a9b65c018
Keiichiro Ui <keiichiro.ui@gmail.com>
2013-03-03 23:31:41 +0900

    fix install script

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ BASE_DIR=$(cd `dirname $0` && pwd)
 CONFIG="$BASE_DIR/etc/ddns-updater"
 INIT="$BASE_DIR/etc/init.d/ddns-updater"
 
-install --compare --backup --owner root --group root --mode 600 $CONFIG /etc
+[ ! -e "/etc/`basename $CONFIG`" ] \
+    && install --backup --owner root --group root --mode 600 $CONFIG /etc
 install --backup --owner root --group root --mode 755 $INIT /etc/init.d
 update-rc.d ddns-updater defaults
